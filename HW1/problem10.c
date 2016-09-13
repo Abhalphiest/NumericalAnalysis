01f1e05e44b0e492e71a370801bd139be5c7ea8a
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-09-13 19:07:01 -0400

    wrote the code for problem 10

--- HW1/problem10.c
+++ HW1/problem10.c
@@ -5,8 +5,8 @@
  *
  * Homework 1 Problem 10
  *
- * This file contains a c program that finds the maximum value for spectral
- * energy density for a given temperature T.
+ * This file contains a c program that finds the x that generates maximum value for spectral
+ * energy density.
  *
  */
 
@@ -18,7 +18,14 @@
 double f(double x)
 {
  //first derivative equation
- return v*(B+w)*(B+w) - S*(B+2*w)/(w*w) - Q;
+ return -(exp(x)*(x-3)+3)*x^2/pow((exp(x)-1),2);
+}
+double f_prime(double x)
+{
+ //second derivative equation
+ return (2*exp(x)*(exp(x)*(x-3)+3)*x*x)/pow((exp(x)-1),3)-
+		((exp(x)*(x-3)+exp(x))*x^2)/(pow(exp(x)-1),2)-
+		(2*(exp(x)*(x-3)+3)*x)/pow(e^x-1,2);
 }
 int main()
 {
@@ -26,7 +33,7 @@ int main()
  printf("Enter the initial guess for the root:");
  scanf("%lf",&x_0);
  header("1D Newton's Method");
- double w =  newton1D(&g,&g_prime,x_0,1000);
+ double w =  newton1D(&f,&f_prime,x_0,1000);
 
  return 0; //exit success
 }
