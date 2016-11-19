4380da0eed3e62c81c8ce10992e10fd46133dbc2
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-11-19 13:13:06 -0500

    did some work on problem 3

--- HW3/problem3.c
+++ HW3/problem3.c
@@ -10,10 +10,14 @@
 #include"approximations.h"
 #include<stdio.h>
 
-double* magToLuminosity( double* y, int n);
+Point* magtoLuminosity( Point* points, int n);
 
 double* x;
 double* y;
+double* magnitudespline;
+double* luminosityspline;
+Point* points;
+Point* luminosities;
 int n;
 
 double f(double val)
@@ -26,6 +30,17 @@ double f(double val)
  //shouldn't ever fire ideally
  printf("non-data value requested \n");
  return 0; //garbage value
+}
+
+double magf(double val)
+{
+ double mag = splint(magnitudespline,n,points,val);
+ return 85.5*pow(10,-.4*mag);
+}
+
+double lumf(double val)
+{
+ return splint(luminosityspline,n,luminosities,val);
 }
 
 int main()
@@ -45,28 +60,47 @@ int main()
  simpresult = simpson(f,0,1,5);
  trapresult = trapezoid(f,0,1,10);
 
- printf("Simpson's: %lf \t Trapezoid: %lf \n");
+ printf("Simpson's: %lf \t Trapezoid: %lf \n",simpresult, trapresult);
 
+ points = (Point*) malloc(sizeof(Point)*n);
+ for(int i = 0; i < n; i++)
+ {
+  points[i].x = x[i];
+  points[i].y = y[i];
+ }
  //construct natural cubic spline
-
-
-
+ magnitudespline = cubicSpline(n, NATURAL, 0,0, points);
+ //converted to luminosity in the function given to simpson
  //integrate numerically
- 
-
- //convert to luminosity
-
+ double magnituderesult = simpson(magf,0,1,20);
  
  //convert data to luminosities
- double* luminosities;
- luminosities = magtoLuminosity(y,n);
- 
-
+ luminosities = magtoLuminosity(points,n);
+  
  //make cubic spline
-
-
+ luminosityspline = cubicSpline(n,NATURAL,0,0,luminosities);
  //integrate numerically
-
- 
+ double luminosityresult = simpson(lumf,0,1,20);
  //print results
+ printf("Total Emitted Energy from Magnitude Spline: %lf\n",magnituderesult);
+ printf("Total Emitted Energy from Luminosity Spline: %lf\n",luminosityresult);
+
+ //free memory
+ free(x);
+ free(y);
+ free(points);
+ free(luminosities);
+ free(magnitudespline);
+ free(luminosityspline);
+}
+
+Point* magtoLuminosity(Point* points, int n)
+{
+ Point* luminosities = (Point*) malloc(sizeof(Point)*n);
+ for(int i = 0; i < n; i++)
+ {
+  luminosities[i].x = points[i].x;
+  luminosities[i].y = 85.5*pow(10,-.4*points[i].y);
+ }
+ return luminosities;
 }
