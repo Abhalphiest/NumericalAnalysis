88ac5c2a9a1606b8c5fad7c2fe77b3dae771acb1
Margaret Dorsey <margaretdorsey94@gmail.com>
2016-12-02 20:57:35 -0500

    homework 4 code needs debugging

--- HW4/homework4-123.c
+++ HW4/homework4-123.c
@@ -28,7 +28,7 @@ double prob3f(double t, double y)
 //exact solutions for the ODEs to check error
 double prob1sol(double t)
 {
-	return 1/25.0*(exp(-2*t)*(exp(5*t)*(5*t - 1) + 1);
+	return 1/25.0*(exp(-2*t)*(exp(5*t)*(5*t - 1) + 1));
 }
 double prob2sol(double t)
 {
@@ -37,26 +37,27 @@ double prob2sol(double t)
 }
 double prob3sol(double t)
 {
-	return .5*pow(t,-2)*(-cos(2*t)+4+cos(2);
+	return .5*pow(t,-2)*(-cos(2*t)+4+cos(2));
 }
 int main()
 {
 	int numpoints;
 	printf("Enter the number of points to evaluate:");
 	scanf("%d",&numpoints);
-	double step = 1.0/numpoints;
+	double ptstep = 1.0/numpoints;
 	double errbound = pow(10,-6);
 	double x,y,step;
 	int flag = 0;
 	//eq 1
 	int nEuler_1, nRK2_1, nRK4_1;
-	
+	printf("eq 1\n");	
 	//euler
 	nEuler_1 = 0;
-	while(flag == 0)
+	/*while(flag == 0)
 	{
 		nEuler_1++;
 		step = 1.0/nEuler_1;
+		printf("%lf\n",step);
 		flag = 1;
 		x = 0;
 		for(int i = 0; i < numpoints; i++)
@@ -64,11 +65,14 @@ int main()
 			y = Euler(prob1f,0,0,x,step);
 			if(fabs(y-prob1sol(x))>errbound)
 				flag = 0;
-			x += step;
+			printf("%lf %lf \n",y,prob1sol(x));
+			x += ptstep;
 		}
 		
 	}
-	
+	*/
+	flag = 0;
+	printf("RK2\n");	
 	//RK2
 	nRK2_1 = 0;
 	while(flag == 0)
@@ -82,11 +86,11 @@ int main()
 			y = RK2(prob1f,0,0,x,step);
 			if(fabs(y-prob1sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-	
+	flag =0;
 	//RK4
 	nRK4_1 = 0;
 	while(flag == 0)
@@ -100,14 +104,14 @@ int main()
 			y = RK4(prob1f,0,0,x,step);
 			if(fabs(y-prob1sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-
+//	flag = 0;
 	//eq 2
 	int nEuler_2, nRK2_2, nRK4_2;
-	
+	printf("eq2\n");
 	//euler
 	nEuler_2 = 0;
 	while(flag == 0)
@@ -115,17 +119,17 @@ int main()
 		nEuler_2++;
 		step = 1.0/nEuler_2;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = Euler(prob2f,0,0,x,step);
+			y = Euler(prob2f,1,2,x,step);
 			if(fabs(y-prob2sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-	
+	flag = 0;
 	//RK2
 	nRK2_2 = 0;
 	while(flag == 0)
@@ -133,17 +137,17 @@ int main()
 		nRK2_2++;
 		step = 1.0/nRK2_2;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = RK2(prob2f,0,0,x,step);
+			y = RK2(prob2f,1,2,x,step);
 			if(fabs(y-prob2sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-	
+	flag = 0;
 	//RK4
 	nRK4_2 = 0;
 	while(flag == 0)
@@ -151,20 +155,20 @@ int main()
 		nRK4_2++;
 		step = 1.0/nRK4_2;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = RK4(prob2f,0,0,x,step);
+			y = RK4(prob2f,1,2,x,step);
 			if(fabs(y-prob2sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-
+//	flag = 0;
 	//eq 3
 	int nEuler_3, nRK2_3, nRK4_3;
-	
+	printf("eq3\n");
 	//euler
 	nEuler_3 = 0;
 	while(flag == 0)
@@ -172,17 +176,17 @@ int main()
 		nEuler_3++;
 		step = 1.0/nEuler_3;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = Euler(prob3f,0,0,x,step);
+			y = Euler(prob3f,1,2,x,step);
 			if(fabs(y-prob3sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-	
+	flag = 0;
 	//RK2
 	nRK2_3 = 0;
 	while(flag == 0)
@@ -190,17 +194,17 @@ int main()
 		nRK2_3++;
 		step = 1.0/nRK2_3;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = RK2(prob3f,0,0,x,step);
+			y = RK2(prob3f,1,2,x,step);
 			if(fabs(y-prob3sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
-	
+	flag = 0;
 	//RK4
 	nRK4_3 = 0;
 	while(flag == 0)
@@ -208,13 +212,13 @@ int main()
 		nRK4_3++;
 		step = 1.0/nRK4_3;
 		flag = 1;
-		x = 0;
+		x = 1;
 		for(int i = 0; i < numpoints; i++)
 		{
-			y = RK4(prob3f,0,0,x,step);
+			y = RK4(prob3f,1,2,x,step);
 			if(fabs(y-prob3sol(x))>errbound)
 				flag = 0;
-			x += step;
+			x += ptstep;
 		}
 		
 	}
