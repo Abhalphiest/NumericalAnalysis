ed2cecdfade5ac83a4fba7e50bc6316bf49591e4
Margaret Dorsey <margaretdorsey94@gmail.com>
2017-11-05 12:23:22 -0500

    cleaned up repos to only include work that is relevant/useful after the end of the class

--- HW2/problem5.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include <stdlib.h> //for quick sort
-
-void evenPoints(double xstart, double xend);
-void printPoints(Point* points, int n);
-Point* findActual(Point* points, int n);
-Point* makePoints(double xstart,double xend, int npoints);
-double* chebyshevMidpoints(double xstart,double xend, int n);
